c8e3d4fe4b22eb0ee94bcc33e1bdcbac226bea1e
Brendan Molloy <brendan@bbqsrc.net>
2015-04-08 02:05:10 +0200

    oh

---
+++ utils.py
@@ -0,0 +1,43 @@
+import datetime
+import json
+import re
+import uuid
+import logging
+import time
+
+import pymongo
+from pymongo import Connection
+
+def safe_modify(col, query, update, upsert=False):
+    for attempt in range(5):
+        try:
+            result = col.find_and_modify(
+                    query=query,
+                    update=update,
+                    upsert=upsert,
+                    new=True
+            )
+            return result
+        except pymongo.errors.OperationFailure:
+            return False
+        except pymongo.errors.AutoReconnect:
+            wait_t = 0.5 * pow(2, attempt)
+            time.sleep(wait_t)
+    return False
+
+
+def safe_insert(collection, data):
+    for attempt in range(5):
+        try:
+            collection.insert(data, safe=True)
+            return True
+        except pymongo.errors.OperationFailure:
+            return False
+        except pymongo.errors.AutoReconnect:
+            wait_t = 0.5 * pow(2, attempt)
+            time.sleep(wait_t)
+    return False
+
+def find_by_email(emails):
+    return [x['_id'].hex for x in Connection().ppau.members.find({
+        "details.email": { "$in": emails }}))]
